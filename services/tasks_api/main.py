cac31b6a6cd7fe75d93150834766bc0a3ae7f0e5
jl.tavera <jl.tavera@uniandes.edu.co>
2024-03-19 23:21:48 -0500

    Deployment updates

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -1,8 +1,10 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins="*",
@@ -14,6 +16,7 @@ app.add_middleware(
 
 @app.get("/api/health-check/")
 def health_check():
+
     return {"message": "OK"}
 
 handle = Mangum(app) 
